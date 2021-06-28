40475194aa8e1053eb4dc86a439e8b5cf62b066d
Bharani <bharanitechv@gmail.com>
2021-06-28 16:21:38 +0530

    initial Redux code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+
+
+
+//Action - Increment 
+const incrementCounter = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrementCounter = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+//Reducer - Handle state change
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case "INCREMENT" : 
+    return state + 1
+    case "DECREMENT" : 
+    return state - 1
+  }
+}
+
+//Store - Globalize state
+let store = createStore(counter)
+
+//console
+store.subscribe(() => { console.log("state--->", store.getState())})
+
+store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+
+
+
+
+
+//Dispatch - Dispatch the action
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +53,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
